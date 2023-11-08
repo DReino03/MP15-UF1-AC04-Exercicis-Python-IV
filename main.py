db7eaf044bdd4b13f885652d00feed4b1a624ecd
daniel [itb] reinosa luque <daniel.reinosa.7e6@itb.cat>
2023-11-08 17:17:15 +0100

    si

--- main.py
+++ main.py
@@ -3,7 +3,6 @@ import numpy as np
 import matplotlib.pyplot as plt
 import csv
 
-
 def load_data(file_name):
     file = open(file_name, "r")
     reader = csv.reader(file)
@@ -13,37 +12,22 @@ def load_data(file_name):
     file.close()
     return array
 
-estacions = load_data("MeteoCat_Estacions_2020.csv")
-detall = load_data("2022_MeteoCat_Detall_Estacions.csv")
-metadata = load_data("MeteoCat_Metadades.csv")
-
-
-#Calculate avg with acronim
-def calculate_avg(valor:str) -> list:
-    day = {}
-    for row in detall:
-        date = row[0]
-        acronim = row[3]
-        day = date.split("-")[2]
-        month = date.split("-")[1]
-
-        if month == "02" and acronim == valor.upper():
-            num_day = int(day)
-            value = float(row[4])
-            if num_day not in day:
-                day[num_day] = []
-            day[num_day].append(value)
-    avg = []
-    for dia, value in day.items():
-        avg = sum(value) / len(value)
-        avg.append((dia, avg))
-                
-    return avg
-
+estacions = load_data("./MeteoCat_Estacions_2020.csv")
+detall = load_data("./2022_MeteoCat_Detall_Estacions.csv")
+metadata = load_data("./MeteoCat_Metadades.csv")   
 
 # Temperatura media de febrero de 2020
 def avg_temp_February():
     avg =  calculate_avg("TM")
+    plt.plot(avg)
+    plt.xlabel('Days')
+    plt.ylabel('Temperature')
+    plt.title('Media diaria de febrero')
+    plt.legend()
+    plt.grid(True)
+    plt.tight_layout()
+    plt.show()
+    '''
     print(np.array(avg))
     plt.plot(np.array(avg)[:,0], np.array(avg)[:,1])
     plt.ylabel("Temperatura ºC")
@@ -51,14 +35,38 @@ def avg_temp_February():
     plt.xticks(range(1,29))
     plt.title("Temperatura media de febrero de 2020")
     plt.show()
+    '''
 
+#Calculate avg with acronim
+def calculate_avg(average:str) -> list:
+    days = {}
+    for row in detall:
+        date = row[0]
+        acronim = row[3]
+        day = date.split("-")[2]
+        month = date.split("-")[1]
+
+        if month == "02" and acronim == average.upper():
+            num_day = int(day)
+            value = float(row[4])
+            if num_day not in days:
+                days[num_day] = []
+            days[num_day].append(value)
+    avg = []
+    for dia, value in days.items():
+        avg = sum(value) / len(value)
+        avg.append((dia, avg))
+                
+    return avg
+
+"""
 def predict_temp_February_2023():
-    
     avg = calculate_avg("TM")
     temp_feb_2022 = np.array(avg)[:,1]
-    
+"""
 
 def main():
+
     while True:
         print("Selecciona un ejercicio:")
         print("1. Cargar Datos")
@@ -69,12 +77,22 @@ def main():
         opcion = input("Ingresa el número del ejercicio: ")
 
         if opcion == '1':
-            load_data()
+            detall
+            estacions
+            metadata
+            print("Datos cargados correctamente.")
+            
         elif opcion == '2':
-            print(avg_temp_February())
-        elif opcion == '3':
+            avg_temp_February()
 
+        elif opcion == '3':
+            print("¡Adiós!")
+            break
+            
         elif opcion == '4':
+            print("¡Adiós!")
+            break
+
         elif opcion == '5':
             print("¡Adiós!")
             break
