7ae1f4eca7e486dd2fb2942cf9d0af9ac92630aa
oscar [itb] perona gomez <oscar.perona.7e6@itb.cat>
2023-11-11 16:25:20 +0100

    Ex3

--- main.py
+++ main.py
@@ -1,120 +1,102 @@
+import matplotlib.pyplot as plt
 import pandas as pd
 import numpy as np
-import matplotlib.pyplot as plt
-import csv
 
-def load_data(file_name):
-    file = open(file_name, "r")
-    reader = csv.reader(file)
-    header = next(reader)
-    data = [ row for row in reader ]
-    array = np.array(list(data))
-    file.close()
-    return array
 
-estacions = load_data("./MeteoCat_Estacions_2020.csv")
-detall = load_data("./2022_MeteoCat_Detall_Estacions.csv")
-metadata = load_data("./MeteoCat_Metadades.csv")   
+def load_datas(path):
+    return pd.read_csv(path, header=0)
 
-# Temperatura media de febrero de 2020
-def avg_temp_February():
-    """
-    avg =  calculate_avg("TM")
-    plt.plot(np.array(avg)[:,0], np.array(avg)[:,1])
+
+# Lee los ficheros csv
+stationData = load_datas("MeteoCat_Estacions_2020.csv")
+stationDataDetail = load_datas("2022_MeteoCat_Detall_Estacions.csv")
+metaData = load_datas("MeteoCat_Metadades.csv")
+
+# Indica que la columna DATA_LECTURA es una fecha para poder acceder a ello(acceder al mes, al dia...)
+stationDataDetail['DATA_LECTURA'] = pd.to_datetime(stationDataDetail['DATA_LECTURA'])
+
+# Filta los datos del fichero csv para que guardar los datos de frbereo de 2022 y que sean de ACRÒNIM (temperatura)
+februaryData = stationDataDetail[
+    (stationDataDetail['DATA_LECTURA'].dt.month == 2) &
+    (stationDataDetail['DATA_LECTURA'].dt.year == 2022) &
+    (stationDataDetail['ACRÒNIM'] == 'TM')]
+
+# Ordena data frame colocando CODI_ESTACIO, DATA_LECTURA, VALOR en cloumnas diferentes
+stationTemperatures = februaryData.groupby(['CODI_ESTACIO', februaryData['DATA_LECTURA'].dt.day])[
+    'VALOR'].mean().unstack(level=0)
+
+
+def media_febrero_unidas():
+    plt.figure(figsize=(14, 8))
+
+    for station in stationTemperatures.columns:
+        plt.plot(stationTemperatures.index, stationTemperatures[station], label=f'{station}', marker='o')
+
     plt.xlabel('Days')
     plt.ylabel('Temperature')
-    plt.title('Media diaria de febrero')
-    plt.tight_layout()
-    plt.show()
-    """
-
-    detall['DATA_LECTURA'] = pd.to_datetime(detall['DATA_LECTURA'])
-
-    february_data = detall[
-        (detall['DATA_LECTURA'].dt.month == 2) &
-        (detall['DATA_LECTURA'].dt.year == 2022) &
-        (detall['ACRÒNIM'] == 'TM')]
-
-    temperatures_per_station = february_data.groupby(['CODI_ESTACIO', february_data['DATA_LECTURA'].dt.day])['VALOR'].mean().unstack(level=0)
-
-     # Part 1
-    plt.figure(figsize=(10, 6))
-    for station in temperatures_per_station.columns:
-        plt.plot(temperatures_per_station.index, temperatures_per_station[station], label=f'Estación {station}',
-                 marker='o')
-    plt.xlabel('Días de Febrero')
-    plt.ylabel('Temperatura media diaria')
-    plt.title('Comparativa de Temperatura Media Diaria - Febrero 2022')
+    plt.title('Temperaturas medias de febrero segun estacion')
     plt.legend()
     plt.grid(True)
     plt.tight_layout()
-    plt.xticks(temperatures_per_station.index)
+    plt.xticks(stationTemperatures.index)
+    plt.show()
+
+def media_febrero_separadas():
+    fig, axs = plt.subplots(2, 2, figsize=(14, 8))
+    fig.suptitle('Comparativa de Temperatura Media Diaria - Febrero 2022')
+
+    # Establece los títulos para cada subgráfico
+    axs[0, 0].set_title('Estación 1')
+    axs[0, 1].set_title('Estación 2')
+    axs[1, 0].set_title('Estación 3')
+    axs[1, 1].set_title('Estación 4')
+
+    # Grafica cada estación en su subgráfico correspondiente
+    axs[0, 0].plot(stationTemperatures.index, stationTemperatures[stationTemperatures.columns[0]],
+                   label='Estación 1', marker='o', color=plt.cm.tab10(0))
+    axs[0, 1].plot(stationTemperatures.index, stationTemperatures[stationTemperatures.columns[1]],
+                   label='Estación 2', marker='o', color=plt.cm.tab10(1))
+    axs[1, 0].plot(stationTemperatures.index, stationTemperatures[stationTemperatures.columns[2]],
+                   label='Estación 3', marker='o', color=plt.cm.tab10(2))
+    axs[1, 1].plot(stationTemperatures.index, stationTemperatures[stationTemperatures.columns[3]],
+                   label='Estación 4', marker='o', color=plt.cm.tab10(3))
+
+    # Añade etiquetas y leyendas
+    for ax in axs.flat:
+        ax.set(xlabel='Days', ylabel='Temperature')
+        ax.legend()
+        ax.grid(True)
+        ax.set_xticks(stationTemperatures.index)
+        ax.set_ylim(stationTemperatures.min().min() - 1, stationTemperatures.max().max() + 1)
+
+    # Ajusta el diseño y muestra la figura
+    plt.tight_layout()
     plt.show()
 
 
-    
-
-#Calculate avg with acronim
-def calculate_avg(average:str) -> list:
-    days = {}
-    for row in detall:
-        date = row[0]
-        acronim = row[3]
-        day = date.split("-")[2]
-        month = date.split("-")[1]
-
-        if month == "02" and acronim == average.upper():
-            num_day = int(day)
-            value = float(row[4])
-            if num_day not in days:
-                days[num_day] = []
-            days[num_day].append(value)
-    avg = []
-    for dia, values in days.items():
-        avgs = sum(values) / len(values)
-        avg.append((dia, avgs))
-                
-    return avg
-
-"""
-def predict_temp_February_2023():
-    avg = calculate_avg("TM")
-    temp_feb_2022 = np.array(avg)[:,1]
-"""
-
-def main():
-
+def menu():
     while True:
-        print("Selecciona un ejercicio:")
-        print("1. Cargar Datos")
-        print("2. Visualizar Temperatura en Febrero")
-        print("3. Predicción de Temperatura en Febrero 2023")
-        print("4. Predicción de Lluvia en Febrero 2023")
-        print("5. Salir")
-        opcion = input("Ingresa el número del ejercicio: ")
+        print("MENU:")
+        print("1. Ver temperatura media de febrero en un solo gráfico")
+        print("2. Ver temperatura media de febrero en diferentes gráficos")
+        print("3. Predicción de temperatura del mes de febrero")
+        print("4. Predicción de lluvias del mes de febrero")
+        option = input("Por favor, seleccione una opción: ")
 
-        if opcion == '1':
-            detall
-            estacions
-            metadata
-            print("Datos cargados correctamente.")
-            
-        elif opcion == '2':
-            avg_temp_February()
+        if option == "1":
+            media_febrero_unidas()
 
-        elif opcion == '3':
-            print("¡Adiós!")
-            break
-            
-        elif opcion == '4':
-            print("¡Adiós!")
-            break
+        elif option == "2":
+            media_febrero_separadas()
 
-        elif opcion == '5':
-            print("¡Adiós!")
-            break
+        elif option == "3":
+            print("Seleccionaste la opción 3")
+
+        elif option == "4":
+            print("Seleccionaste la opción 4")
+
         else:
-            print("Opción no válida. Ingresa un número del 1 al 5.")
+            print("Opción no válida. Por favor, ingrese una opcion correcta")
 
-if __name__ == "__main__":
-    main()
+
+menu()
